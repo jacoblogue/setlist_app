9737df20b634b57bb6c81ff57ea78c0e158198c1
Jacob Logue <jakelogue84@gmail.com>
2022-07-09 13:32:34 -0400

    move components into separate directory

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { MemberList } from './MemberList';
-import { TextField } from "./TextField"
+import { MemberList } from './components/MemberList';
+import { TextField } from "./components/TextField"
 
 const App = () => {
   return (
