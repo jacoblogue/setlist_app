e873f871d42ff1cf74e060a86af85067f7946a5f
Jacob Logue <jakelogue84@gmail.com>
2022-07-10 13:28:37 -0400

    initial commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { MemberList } from './components/MemberList';
-import { TextField } from "./components/TextField"
 
 const App = () => {
   return (
