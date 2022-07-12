51e9011c0a9a08633eeb762190ebea2799be5ca9
Jacob Logue <jakelogue84@gmail.com>
2022-07-12 15:58:31 -0400

    add method to create song

--- flask-server/app/routes.py
+++ flask-server/app/routes.py
@@ -1,7 +1,8 @@
 import os
 from . import create_app
+from . import db
 from .models import Song
-from flask import abort, jsonify
+from flask import abort, jsonify, request
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 
@@ -18,3 +19,37 @@ def show_song(id):
     if song is None:
         abort(404)
     return jsonify(song.to_json())
+
+
+@app.route('/song/<int:id>', methods=['DELETE'])  # delete song by id
+def delete_song(id):
+    song = Song.query.get(id)
+    if song is None:
+        abort(404)
+    db.session.delete(song)
+    db.session.commit()
+    return jsonify({'result': True})
+
+
+@app.route('/song', methods=['POST'])  # create song
+def create_song():
+    if not request.json:
+        abort(400)
+    song = Song(artist=request.json['artist'],
+                title=request.json['title'],
+                length=request.json['length']
+                )
+    db.session.add(song)
+    db.session.commit()
+    return jsonify(song.to_json()), 201
+
+# @app.route('/song/<int:id>', methods=['PUT'])  # update song by id
+# def update_song(id):
+#     song = Song.query.get(id)
+#     if song is None:
+#         abort(404)
+#     song.title = request.json.get('title', song.title)
+#     song.artist = request.json.get('artist', song.artist)
+#     song.length = request.json.get('length', song.length)
+#     db.session.commit()
+#     return jsonify(song.to_json())
