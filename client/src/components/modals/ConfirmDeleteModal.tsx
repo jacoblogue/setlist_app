71ed7fe0eaf509633e12556fd8ffdd4f445d8ee1
Jacob Logue <jakelogue84@gmail.com>
2022-07-20 15:52:47 -0400

    add react-modal types

--- client/src/components/modals/ConfirmDeleteModal.tsx
+++ client/src/components/modals/ConfirmDeleteModal.tsx
@@ -1,2 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
+import ReactModal from 'react-modal'
+ReactModal.setAppElement('#root')
 
+export const ConfirmDeleteModal: React.FC = () => {
+  const [isOpenStatus, setIsOpenStatus] = useState(false)
+
+  return (
+    <ReactModal isOpen={isOpenStatus}>
+
+    </ReactModal>
+  )
+}
