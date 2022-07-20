e60ca858382496a6cfdcadee56976b45ed357b6f
Jacob Logue <jakelogue84@gmail.com>
2022-07-20 16:10:38 -0400

    add confirm delete modal

--- client/src/components/modals/ConfirmDeleteModal.tsx
+++ client/src/components/modals/ConfirmDeleteModal.tsx
@@ -3,8 +3,6 @@ import ReactModal from 'react-modal'
 ReactModal.setAppElement('#root')
 
 export const ConfirmDeleteModal: React.FC<any> = ({modalOpenStatus}) => {
-  // const [isOpenStatus, setIsOpenStatus] = useState(false)
-
   return (
     <ReactModal isOpen={modalOpenStatus}>
       <h1>Test Nation</h1>
