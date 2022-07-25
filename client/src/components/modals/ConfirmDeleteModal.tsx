7c9d12e06c7ebfb63f63c579da45425d5c6b9c4d
Jacob Logue <jakelogue84@gmail.com>
2022-07-25 14:38:46 -0400

    add interface for props

--- client/src/components/modals/ConfirmDeleteModal.tsx
+++ client/src/components/modals/ConfirmDeleteModal.tsx
@@ -2,10 +2,17 @@ import React, { useState } from "react";
 import ReactModal from 'react-modal'
 ReactModal.setAppElement('#root')
 
-export const ConfirmDeleteModal: React.FC<any> = ({modalOpenStatus}) => {
+interface Props {
+  modalOpenStatus: boolean
+}
+
+export const ConfirmDeleteModal: React.FC<Props> = ({modalOpenStatus}) => {
   return (
     <ReactModal isOpen={modalOpenStatus}>
-      <h1>Test Nation</h1>
+      <h1>Are you sure?</h1>
+      <p>Are you sure you want to delete this song?</p>
+      <button>Confirm</button>
+      <button>Cancel</button>
     </ReactModal>
   )
 }
