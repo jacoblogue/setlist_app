91ef72e0109fee2ccf76f7dbc1ce950bf3f202b1
Jacob Logue <jakelogue84@gmail.com>
2022-07-25 15:07:24 -0400

    replace react-modal with react-bootstrap modal

--- client/src/components/modals/ConfirmDeleteModal.tsx
+++ client/src/components/modals/ConfirmDeleteModal.tsx
@@ -1,7 +1,5 @@
 import React, { useState } from "react";
-import ReactModal from 'react-modal'
-import {Button} from 'react-bootstrap'
-ReactModal.setAppElement('#root')
+import {Button, Modal} from 'react-bootstrap'
 
 interface Props {
   modalOpenStatus: boolean
@@ -9,11 +7,11 @@ interface Props {
 
 export const ConfirmDeleteModal: React.FC<Props> = ({modalOpenStatus}) => {
   return (
-    <ReactModal isOpen={modalOpenStatus}>
+    <Modal show={modalOpenStatus}>
       <h1>Are you sure?</h1>
       <p>Are you sure you want to delete this song?</p>
-      <button>Confirm</button>
-      <Button variant="btn btn-primary">Cancel</Button>
-    </ReactModal>
+      <Button variant="outline-primary">Confirm</Button>
+      <Button variant="outline-primary">Cancel</Button>
+    </Modal>
   )
 }
